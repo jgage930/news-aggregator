1e7a69737109722b28d1d7d8839a61251680935c
jgage930 <jgage930@gmail.com>
2022-06-08 09:22:38 -0500

    added templates

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
 # flask set up
@@ -21,7 +21,7 @@ class Article(db.Model):
 
 @app.route('/')
 def index():
-	pass
+	return render_template('index.html')
 
 if __name__ == '__main__':
 	app.run(debug=True)
