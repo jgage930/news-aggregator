1cec3d4e792a0da09783424dc20917bdcd806ae2
jgage930 <jgage930@gmail.com>
2022-06-09 22:09:37 -0500

    added article view

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
+from parsers import CnnParser, NprParser
 
 # flask set up
 app = Flask(__name__)
-app.config['SQLALCHEM_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 
 # database
 db = SQLAlchemy(app)
@@ -17,11 +18,38 @@ class Article(db.Model):
 	authors = db.Column(db.Text)
 	orig_article_link = db.Column(db.Text)
 	content = db.Column(db.Text)
-	source = db.Column(db.String(15))
+	source = db.Column(db.String(50))
+	published_date = db.Column(db.String(100))
+	image_url = db.Column(db.Text)
+
+# gets the article data from feeds and adds to database
+# plan to run this on a cron or some sort of regular basis
+def getArticles():
+	cnn = CnnParser()
+	npr = NprParser()
+
+	cnn_data = cnn.parse()
+	for dict in cnn_data:
+		article = Article(**dict)
+		db.session.add(article)
+	
+	# TODO must debug npr_parser not motivated at the moment
+	# npr_data = npr.parse()
+	# for dict in npr_data:
+	# 	article = Article(**dict)
+	# 	db.session.add(article)
+
+	db.session.commit()
 
 @app.route('/')
 def index():
 	return render_template('index.html')
+
+@app.route('/article/<int:article_id>')
+def article_view(article_id):	
+	article = Article.query.filter_by(id=article_id).one()
+
+	return render_template('article_view.html', article=article)
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- parsers.py
+++ parsers.py
@@ -1,3 +1,4 @@
+import dataclasses
 import feedparser as fp
 import requests
 from bs4 import BeautifulSoup as bs
@@ -10,18 +11,24 @@ class CnnParser:
 		# parse all from the politics feed
 		cnn_politics = fp.parse(cnn_feed)
 		self.entries = cnn_politics.entries
+		#self.filter()
+
+	def getTitle(self, entry):
+		return entry['title']
+
+	def getSummary(self, entry):
+		text = entry['summary']
+		tag_start = text.index('<')
+		return text[0:tag_start]
 	
-	def getTitles(self) -> list:
-		return [entry['title'] for entry in self.entries]
-		
-	def getEntries(self):
-		return self.entries
+	def getArchiveDate(self):
+		return datetime.now()
 
-	def getSummaries(self):
-		return [entry['value'] for entry in self.entries]
+	def getOrigArticleUrl(self, entry):
+		return entry['link']
 
-	def getContent(self, link_to_article):
-		data = requests.get(link_to_article)
+	def getContent(self, entry):
+		data = requests.get(self.getOrigArticleUrl(entry))
 
 		soup = bs(data.text, 'html.parser')
 		p_tags = soup.find_all('div', class_="zn-body__paragraph")
@@ -29,39 +36,113 @@ class CnnParser:
 		content = [tag.text for tag in p_tags]
 		return '\n'.join(content)
 
-	def getLinks(self):
-		return [entry['id'] for entry in self.entries]
+	def getPublishedDate(self, entry):
+		if 'published' in entry:
+			return entry['published']
+		return self.getArchiveDate().strftime('%d %M %Y')
 
-	def getAuthors(self, link_to_article):
-		data = requests.get(link_to_article)
+	def getAuthors(self, entry):
+		data = requests.get(self.getOrigArticleUrl(entry))
 
 		soup = bs(data.text, 'html.parser')
 		tag = soup.find('span', class_='metadata__byline__author')
 
-		return tag.text
+		if tag:
+			return tag.text
+
+	def getImageUrl(self, entry):
+		if 'media_content' in entry:
+			images = entry['media_content']
+			if not images:
+				return 
+			if images[0]['medium'] == 'image':
+				return images[0]['url']
+		return ' '
 
 	# for every article get the required data and return a list of dicts
 	def parse(self):
 		data = []
 		for entry in self.entries:
-			title = entry['title']
-			summary = entry['summary']
-			archive_date = datetime.now()
-			authors = None # need to add
-			orig_article_link = entry['id']
-			content = self.getContent(orig_article_link)
-
 			dict = {
-				'title': title,
-				'summary': summary,
-				'archive_date': archive_date,
-				'authors': authors,
-				'orig_article_link': orig_article_link,
-				'content': content,
-				'source': 'CNN'
+				'title': self.getTitle(entry),
+				'summary': self.getSummary(entry),
+				'archive_date': self.getArchiveDate(),
+				'authors': self.getAuthors(entry),
+				'orig_article_link': self.getOrigArticleUrl(entry),
+				'content': self.getContent(entry),
+				'source': 'CNN',
+				'published_date': self.getPublishedDate(entry),
+				'image_url': self.getImageUrl(entry)
 			}
 			data.append(dict)
 		return data
 
-test = CnnParser()
-entries = test.entries
+class NprParser:
+
+	def __init__(self) -> None:
+		npr_feed = 'https://feeds.npr.org/1001/rss.xml'
+		# parse all from the politics feed
+		npr_politics = fp.parse(npr_feed)
+		self.entries = npr_politics.entries
+		#self.filter()
+
+	def filter(self):
+		valid_keys = {
+			'title',
+			'summary',
+			'id',
+			'media_content',
+			'author',
+			#'published'
+		}
+
+		for entry in self.entries[:]:
+			if not valid_keys.issubset(set(entry.keys())):
+				self.entries.remove(entry)
+
+	def getContent(self, link_to_article):
+		data = requests.get(link_to_article)
+
+		soup = bs(data.text, 'html.parser')
+		p_tags = soup.find_all('div', id="storytext")
+		
+		content = [tag.text for tag in p_tags]
+		return '\n'.join(content)
+
+	def getAuthors(self, link_to_article):
+		data = requests.get(link_to_article)
+
+		soup = bs(data.text, 'html.parser')
+		tags = soup.find_all('p', class_='byline__name byline__name--block')
+
+		content = [tag.text.strip() for tag in tags]
+		return ' '.join(content)
+
+	def getImageUrl(self, entry):
+		content = entry['content']
+		if not content:
+			return
+		
+		value = content[0]['value']
+		src_index = value.index('src=')
+		closed_index = value.find('/>')
+		url = value[src_index + 4: closed_index]
+		return url
+
+	def parse(self):
+		data = []
+		for entry in self.entries:
+			dict = {
+				'title': entry['title'],
+				'summary': entry['summary'],
+				'archive_date': datetime.now(),
+				'authors': entry['author'],
+				'orig_article_link': entry['id'], # confirm
+				'content': self.getContent(),
+				'source': 'NPR',
+				'published_date': entry['published'],
+				'image_url': self.getImageUrl()
+			}
+			data.append(dict)
+
+		return data
